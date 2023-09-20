2f28e74ffc8ebc612d06359c50ed0d35283353b1
Vinicius <vfpv@hotmail.com>
2023-09-19 21:53:37 -0300

    Adicionando cabecalho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+import Header from '@/components/Header'
 
 export default function Home() {
   return (
     <div>
+      <Header/>
       <h2>alallalal</h2>
     </div>
   )
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import Link from 'next/link'
+import React from 'react'
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  padding: 40px 5%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const LinkCabecalho = styled(Link)`
+  text-decoration: none;
+  color: var(--gray);
+  font-weight: bold;
+  margin-left: 15px;
+  margin-right: 15px;
+
+  &:hover {
+    color: var(--dark-violet);
+  }
+`
+
+const Botao = styled.button`
+  color: var(--white);
+  background: var(--cyan);
+  border-radius: 20px;
+  padding: 12px;
+  border: none;
+  font-size: 16px;
+  font-weight: bold;
+  &:hover{
+    opacity: 0.5;
+  }
+`
+const LinksLogo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const LinksSing = styled.div`
+
+`
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <LinksLogo>
+        <img src="/images/logo.svg" alt="logo" />
+        <LinkCabecalho href='/'>Features</LinkCabecalho>
+        <LinkCabecalho href='/'>Pricing</LinkCabecalho>
+        <LinkCabecalho href='/'>Resources</LinkCabecalho>
+      </LinksLogo>
+      <LinksSing>
+        <LinkCabecalho href='/'>Login</LinkCabecalho>
+        <Botao>Sign Up</Botao>
+      </LinksSing>
+    </HeaderContainer>
+  )
+}
+
+export default Header
